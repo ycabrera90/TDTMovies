4a01ab4e6602f2daf7d21c6baf2f713a6e3ec22f
yosniel <yosniel.ch@gmail.com>
2022-11-28 21:42:29 -0300

    ci: some configuration was added for load Image from external urls

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    // formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
