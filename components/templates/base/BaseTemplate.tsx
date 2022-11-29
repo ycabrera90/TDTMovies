549c23c39a91290f87669cabd2f2e4194dbcf378
yosniel <yosniel.ch@gmail.com>
2022-11-28 21:53:28 -0300

    feat: a template for components was added

---
+++ components/templates/base/BaseTemplate.tsx
@@ -0,0 +1,18 @@
+// v1.0.4
+import { FC } from "react";
+import styles from "./BaseTemplate.module.scss";
+
+export interface IBaseTemplate {
+  className?: string;
+  sampleTextProp: string;
+}
+
+const BaseTemplate: FC<IBaseTemplate> = ({ className, sampleTextProp }) => {
+  return (
+    <div className={[styles.container, className ? className : ''].join(' ')}>
+      {sampleTextProp}
+    </div>
+  );
+};
+
+export default BaseTemplate;
