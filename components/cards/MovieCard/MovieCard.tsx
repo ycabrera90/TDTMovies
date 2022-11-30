34f379ea4eedbfee1f2f8febdb4d42666c490d83
yosniel <yosniel.ch@gmail.com>
2022-11-30 17:55:05 -0300

    feat: an skeleton was added when image is loading in each card

--- components/cards/MovieCard/MovieCard.tsx
+++ components/cards/MovieCard/MovieCard.tsx
@@ -4,6 +4,7 @@ import { FC, useEffect, useRef, useState } from "react";
 import AddRemButton from "@/components/buttons/AddRemButton/AddRemButton";
 import CSSTransition from "react-transition-group/CSSTransition";
 import styles from "./MovieCard.module.scss";
+import { Skeleton } from "antd";
 
 export interface IMovieCard {
   className?: string;
@@ -15,6 +16,7 @@ export interface IMovieCard {
 
 const MovieCard: FC<IMovieCard> = ({ className, title, overview, imageUrl, voteAverage }) => {
   const [validMovie, setValidMovie] = useState<boolean>(false);
+  const [imageLoading, setImageLoading] = useState<boolean>(true);
   const [cardHeight, setCardHeight ] = useState<number>(307.04);
   const cardDOM = useRef<HTMLInputElement>(null);
   
@@ -36,8 +38,8 @@ const MovieCard: FC<IMovieCard> = ({ className, title, overview, imageUrl, voteA
 
   return (
     <CSSTransition
-      in={validMovie} 
-      timeout={1} 
+      in={validMovie}
+      timeout={1}
       mountOnEnter
       unmountOnExit
       classNames={{ enterDone: styles.entered }}
@@ -47,6 +49,9 @@ const MovieCard: FC<IMovieCard> = ({ className, title, overview, imageUrl, voteA
         ref={cardDOM}
         style={{ height: cardHeight }}
       >
+        {imageLoading && (
+          <Skeleton.Image active={true} className={styles.skeleton} />
+        )}
         <div className={styles['bottom-fog']} />
         <span className={styles.gadge}>{voteAverage}</span>
         <h1 className={styles.title}>{title}</h1>
@@ -59,6 +64,7 @@ const MovieCard: FC<IMovieCard> = ({ className, title, overview, imageUrl, voteA
           sizes="50vw"
           style={{ objectFit: 'cover' }}
           priority
+          onLoadingComplete={() => setImageLoading(false)}
         />
         <AddRemButton className={styles['add-rem-button']} type="add" />
       </article>
