b781c32bb07c984a05c9624aff64a1b64b3387bd
yosniel <yosniel.ch@gmail.com>
2022-12-03 09:27:02 -0300

    feat: when the user go back to the home page, the search input value is reset

--- components/forms/SearchForm/SearchForm.tsx
+++ components/forms/SearchForm/SearchForm.tsx
@@ -1,9 +1,9 @@
 // v1.0.4
-import { BaseSyntheticEvent, FC, useState } from "react";
-import SearchInput from "@/components/inputs/SearchInput/SearchInput";
-import styles from "./SearchForm.module.scss";
-import SearchButton from "@/components/buttons/SearchButton/SearchButton";
 import { useRouter } from "next/router";
+import { BaseSyntheticEvent, FC, useEffect, useState } from "react";
+import SearchInput from "@/components/inputs/SearchInput/SearchInput";
+import SearchButton from "@/components/buttons/SearchButton/SearchButton";
+import styles from "./SearchForm.module.scss";
 
 export interface IBaseTemplate {
   className?: string;
@@ -12,7 +12,6 @@ export interface IBaseTemplate {
 const SearchForm: FC<IBaseTemplate> = ({ className }) => {
   const [queryText, setQueryText] = useState<string>('');
   const router = useRouter();
-
 
   const onChangeInputHandler = (ev: BaseSyntheticEvent) => {
     setQueryText(ev.target.value);
@@ -26,6 +25,13 @@ const SearchForm: FC<IBaseTemplate> = ({ className }) => {
       router.push(`/home`);
     }
   }
+
+  // when the user go back to the home page, the input value is reset
+  useEffect(() => {
+    if(router.pathname === '/home') {
+      setQueryText('');
+    }
+  }, [router.pathname]);
 
   return (
     <form className={[styles.container, className ? className : ''].join(' ')} onSubmit={submitHandler} >
